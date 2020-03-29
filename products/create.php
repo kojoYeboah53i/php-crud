64917f21bc81e0c527e94b6d485aad05c56bd343
kojoYeboah53i <yeboah.determined.isaac@gmail.com>
2020-03-29 12:31:50 +0100

    rest api in php

---
+++ products/create.php
@@ -0,0 +1,101 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// get database connection
+include_once '../api/config/database.php';
+include_once '../api/objects/product.php';
+ 
+$database = new Database();
+$db = $database->getConnection();
+ 
+$product = new Product($db);
+ 
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+
+ //curl method here, please comment out the following to make the above method work or vice
+
+
+//
+// A very simple PHP example that sends a HTTP POST to a remote site
+//
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,"http://www.example.com/tester.phtml");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,
+            "postvar1=value1&postvar2=value2&postvar3=value3");
+
+// In real life you should use something like:
+// curl_setopt($ch, CURLOPT_POSTFIELDS, 
+//          http_build_query(array('postvar1' => 'value1')));
+
+// Receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+
+curl_close ($ch);
+/*
+// Further processing ...
+if ($server_output == "OK") { ... } else { ... }
+?>
+*/
+
+
+
+// make sure data is not empty
+if(
+    !empty($data->name) &&
+    !empty($data->price) &&
+    !empty($data->description) &&
+    !empty($data->category_id)
+){
+ 
+    // set product property values
+    $product->name = $data->name;
+    $product->price = $data->price;
+    $product->description = $data->description;
+    $product->category_id = $data->category_id;
+    $product->created = date('Y-m-d H:i:s');
+ 
+    // create the product
+    if($product->create()){
+ 
+        // set response code - 201 created
+        http_response_code(201);
+ 
+        // tell the user
+        echo json_encode(array("message" => "Product was created."));
+    }
+ 
+    // if unable to create the product, tell the user
+    else{
+ 
+        // set response code - 503 service unavailable
+        http_response_code(503);
+ 
+        // tell the user
+        echo json_encode(array("message" => "Unable to create product."));
+    }
+}
+ 
+// tell the user data is incomplete
+else{
+ 
+    // set response code - 400 bad request
+    http_response_code(400);
+ 
+    // tell the user
+    echo json_encode(array("message" => "Unable to create product. Data is incomplete."));
+}
+
+
+
